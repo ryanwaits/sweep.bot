45b3e7dce75a6377bfa915206f5a66aac63a7b6c
Ryan Waits <ryanwaits@gmail.com>
2017-10-22 09:12:02 -0500

    update connection string

--- app.rb
+++ app.rb
@@ -4,9 +4,9 @@ require 'active_record'
 Dir["./models/*.rb"].each {|file| require file }
 
 # Database Config
-db = URI.parse(ENV['HEROKU_POSTGRESQL_TEAL_URL'] || "postgres://localhost")
+db = URI.parse(ENV['HEROKU_POSTGRESQL_TEAL_URL'] || "postgres://ddruvjsvaaxbyk:d34dc5d5430747972bdaa15a148ef88e1eeb5379ad751cca9ebb1cfc92709c03@ec2-184-73-189-190.compute-1.amazonaws.com:5432/dfes539af18buu")
 ActiveRecord::Base.establish_connection(
-  :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :adapter => 'postgresql',
   :host => db.host,
   :username => db.user,
   :password => db.password,
