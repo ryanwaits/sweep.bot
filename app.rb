82d137716541c184eaa4ca09c012647bfe89a882
Ryan Waits <ryanwaits@gmail.com>
2017-10-22 09:08:52 -0500

    update database connection

--- app.rb
+++ app.rb
@@ -4,8 +4,15 @@ require 'active_record'
 Dir["./models/*.rb"].each {|file| require file }
 
 # Database Config
-
-ActiveRecord::Base.establish_connection(adapter: 'postgresql',  encoding: 'unicode', url: ENV['HEROKU_POSTGRESQL_TEAL_URL'], pool: 5)
+db = URI.parse(ENV['HEROKU_POSTGRESQL_TEAL_URL'] || "postgres://localhost")
+ActiveRecord::Base.establish_connection(
+  :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host => db.host,
+  :username => db.user,
+  :password => db.password,
+  :database => db.path[1..-1],
+  :encoding => 'utf8'
+)
 
 # Talk to Facebook
 get '/webhook' do
